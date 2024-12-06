70175467a192d9473850dbc97792c578f87ab1a0
Blake Woods <yew3@txstate.edu>
2024-12-06 16:04:05 -0600

    change the code up

--- CS Project/backend/server.js
+++ CS Project/backend/server.js
@@ -1,16 +1,24 @@
+// Sets up the server
 const express = require('express');
+
+// Make sure the backend is communicated with the frontend
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 
-// Middleware
 app.use(cors());
 
-// Basic route
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/yourDatabaseName', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// route
 app.get('/', (req, res) => {
   res.send('Welcome to the backend!');
 });
 
-// Start server
+// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
--- CS Project/frontend/src/App.js
+++ CS Project/frontend/src/App.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   const [backendData, setBackendData] = useState('');
@@ -19,20 +18,10 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{backendData}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{backendData}</p>
     </div>
   );
 }
 
 export default App;
+
